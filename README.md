cce78a35aca63fd0080e87148bf215d5495becd7
Jarrett <46313574+jarrett-m@users.noreply.github.com>
2021-12-05 22:59:01 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Legrand_Autologin
-Legrand autologin script for automatic testing.
+IMPORTANT!
+
+Prerequisite:
+pip install webbot
+pip uninstall selenium
+pip install selenium==3.141.0
+A recent version of google chrome.
+
+This script will read the usernames and passwords in the "names_and_pass.txt" and then store if the login was succesful into the log.txt file.
+
+Execution:
+Execute the python file.
+You can adjust username and passwords in the names_and_pass.txt file, format: 
+{USERNAME} {PASSWORD}
+One username and password per line.
+
+Waring:
+This script will open a brower and go to the website automatically, (it can take up to 5-8 seconds for the webpage to first load) it is best to not touch your mouse/keyboard until the script is complete.
+
+Thank you for considering me as a canidate :) this was a lot of fun and have not really done anything like this before!
---
+++ testing_login.py
@@ -0,0 +1,64 @@
+'''An auto login tool for demoblaze.com/index.html'''
+import webbot
+
+def run_login(username, password, web):
+    '''
+        Run login will take in username, password, and browser object
+        to log into the website.
+    '''
+
+    web.click(id="login2")
+    web.implicitly_wait(.25)
+    web.type(username , id='loginusername', classname='form-control')
+    web.implicitly_wait(.10)
+    web.type(password , id='loginpassword')
+    web.implicitly_wait(.10)
+    web.click('Log in')
+    web.implicitly_wait(.10)
+
+    #Try to click "log out", else close alert and go onto next login
+    try:
+        web.click('Log out')
+    except:
+        with open('log.txt', 'a', encoding = 'UTF-8') as logfile:
+            alert = web.switch_to_alert()
+            alert.accept()
+            logfile.write(f"Log in unsuccessful: Username {username} | Password {password}")
+    else:
+        with open('log.txt', 'a', encoding = 'UTF-8') as logfile:
+            logfile.write(f"Log in successful: Username {username} | Password {password}")
+
+    return False
+
+def main():
+    '''
+        Main, launch browser and load webpage.
+    '''
+
+    web = webbot.Browser()
+    web.fullscreen_window()
+    web.go_to('demoblaze.com/index.html')
+    web.implicitly_wait(.5)
+
+    #put names and passwords into a list of tuples
+    n_and_p_list = []
+    with open("names_and_pass.txt", 'r', encoding = 'UTF-8') as n_and_p_file:
+        lines = n_and_p_file.readlines()
+        for line in lines:
+            split_info = line.split(" ")
+            n_and_p_list.append((split_info[0], split_info[1]))
+
+    for item in n_and_p_list:
+        started = False
+        not_done = True
+
+        #Infinte loop until the login process is complete.
+        while not_done:
+            if not started:
+                not_done = run_login(item[0], item[1], web)
+                started = True
+
+    web.quit()
+
+if __name__ == "__main__":
+    main()
